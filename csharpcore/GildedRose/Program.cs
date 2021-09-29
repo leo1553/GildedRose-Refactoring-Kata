dd9afc6a497b8fbf03267fdcf2487c26b6df1574
leo1553 <leonardoleal.qm@hotmail.com.br>
2021-09-29 06:02:16 -0300

    Adicionando injecao de dependencia

--- csharpcore/GildedRose/Program.cs
+++ csharpcore/GildedRose/Program.cs
@@ -1,31 +1,62 @@
-﻿using GildedRoseKata.Data;
+﻿using Autofac;
+using GildedRoseKata.Data;
 using GildedRoseKata.Models;
 using GildedRoseKata.Services;
 using System;
 
 namespace GildedRoseKata {
     public class Program {
-        public static void Main(string[] args) {
+        private IContainer Container { get; set; }
+        private GildedRose GildedRose { get; set; }
+
+        public static void Main(string[] args) => new Program(args).Run();
+
+        public Program(string[] args) {
             Console.WriteLine("OMGHAI!");
+            this.BuildContainer();
+        }
 
-            IDataSource dataSource = new JsonDataSource();
-            IItemDataFactory itemFactory = new ItemDataFactory(
-                new LateQualityUpdateFactory(),
-                new QualityLimiterFactory(),
-                new QualityUpdateFactory(),
-                new SellInUpdateFactory()
-            );
+        private void BuildContainer() {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            // Dados
+            containerBuilder.RegisterType<JsonDataSource>()
+                            .As<IDataSource>();
+            // Fabricas
+            containerBuilder.RegisterType<ItemDataFactory>()
+                            .As<IItemDataFactory>()
+                            .SingleInstance();
+            containerBuilder.RegisterType<LateQualityUpdateFactory>()
+                            .As<ILateQualityUpdateFactory>()
+                            .SingleInstance();
+            containerBuilder.RegisterType<QualityLimiterFactory>()
+                            .As<IQualityLimiterFactory>()
+                            .SingleInstance();
+            containerBuilder.RegisterType<QualityUpdateFactory>()
+                            .As<IQualityUpdateFactory>()
+                            .SingleInstance();
+            containerBuilder.RegisterType<SellInUpdateFactory>()
+                            .As<ISellInUpdateFactory>()
+                            .SingleInstance();
+            // App
+            containerBuilder.RegisterType<GildedRose>();
 
-            var app = new GildedRose(dataSource, itemFactory);
+            this.Container = containerBuilder.Build();
+        }
 
+        private void Run() {
+            this.GildedRose = this.Container.Resolve<GildedRose>();
+            this.Log();
+        }
+
+        private void Log() {
             for(var i = 0; i < 31; i++) {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                foreach(Item item in app.ItemDatas.Keys) {
+                foreach(Item item in this.GildedRose.ItemDatas.Keys) {
                     Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
                 }
                 Console.WriteLine("");
-                app.UpdateQuality();
+                this.GildedRose.UpdateQuality();
             }
         }
     }
